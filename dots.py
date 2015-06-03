9ae6a25b46987b5ac65ae97bdb14936bc0cb6d37
Michael Simpson <mjs2600@gmail.com>
2015-06-02 21:17:43 -0400

    Update dots

--- dots.py
+++ dots.py
@@ -4,11 +4,14 @@ import os.path
 import glob
 import sys
 
-dotfiles = sys.argv[1]
-homedir = sys.argv[2]
+if len(sys.argv) == 2:
+    dotfiles = sys.argv[1]
+else:
+    dotfiles = os.path.dirname(os.path.realpath(__file__))
+homedir = os.path.expanduser("~")
 
-for orig in glob.glob( os.path.join(dotfiles, "dot_*") ):
-    dest = os.path.join( homedir, os.path.split(orig)[-1].replace("dot_", "."))
+for orig in glob.glob(os.path.join(dotfiles, "dot_*")):
+    dest = os.path.join(homedir, os.path.split(orig)[-1].replace("dot_", "."))
     print "Setting up " + dest + " for " + orig
     if os.path.lexists(dest):
         os.remove(dest)
